78e3dbe5b616a503d22f926105e525f0eeddd605
Andeng Mercado <158003221+andengsome@users.noreply.github.com>
2024-11-22 18:29:59 +0800

    Add files via upload

---
+++ Laboratory Activities/Laboratory Activity 3 - 22-11-2024/Artists/ArtistDemo.java
@@ -0,0 +1,28 @@
+public class ArtistDemo {
+    public static void main(String[] args) {
+  
+        Artist artist = new Artist("Charlie Puth", "American", 32, "Music");
+        artist.displayInfo();
+
+        System.out.println();
+
+        Singer singer = new Singer("Nayeon Im", "Korean", 29, "Music", Genre.POP);
+        singer.displayInfo();
+
+        System.out.println();
+
+        Dancer dancer = new Dancer("Bada Lee", "Korean", 29, "Music", DanceStyle.HIPHOP);
+        dancer.displayInfo();
+
+        System.out.println();
+
+        Writer writer = new Writer("Maxine Lat", "Filipino", 37, "Literature", WritingStyle.FICTION);
+        writer.displayInfo();
+
+        System.out.println();
+
+        Painter painter = new Painter("Vincent van Gogh", "Dutch",37, "Art", Medium.OIL);
+        painter.displayInfo();
+
+    }
+}
